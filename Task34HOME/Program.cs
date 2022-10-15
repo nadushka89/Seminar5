d939539e4111b4f87447b8597cc19da2f62dcc33
Надюшка Рыбка <kitti-46@ya.ru>
2022-10-15 21:45:24 +0300

    added Task34

---
+++ Task34HOME/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 34: Задайте массив заполненный случайными 
+положительными трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+int[] RandomArray()
+{
+    int[] array = new int[4];
+    Random rnd = new Random();
+    for (int i = 0; i < 4; i++)
+        array[i] = rnd.Next(100, 1000);
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < 4; i++)
+        Console.Write($"{array[i]} ");
+}
+
+int NumbEven(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < 4; i++)
+        if ((array[i] % 2) == 0)
+            counter++;
+    return counter;
+}
+int[] arr = RandomArray();
+PrintArray(arr);
+Console.WriteLine("");
+int count = NumbEven(arr);
+Console.WriteLine($"Количество четных чисел в массиве - {count} ");
+
+
